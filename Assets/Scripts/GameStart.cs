708fba6a4bbbd76aa02e5bb16fc22c000ed2f23b
Corey Perrigo <corey@perrigogames.com>
2017-12-18 00:46:19 -0800

    add LeanTween add a main menu reorganized the basic assets

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameStart : MonoBehaviour {
+	
+	public GameObject MainMenu;
+	
+	// Use this for initialization
+	void Start () {
+		LeanTween.alphaCanvas(MainMenu.GetComponent<CanvasGroup>(), 0, 0f);
+		Invoke("ShowMenu", 3);
+	}
+
+	void ShowMenu() {
+		LeanTween.alphaCanvas(MainMenu.GetComponent<CanvasGroup>(), 1, 0.5f);
+	}
+}
